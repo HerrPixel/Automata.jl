3dd7c5610ffe2826dfc9973693a7c594d7aa7f35
Jonas Seiler <Me@jonasseiler.de>
2023-02-08 19:14:41 +0100

    added function to remove unreachable states from automaton

--- src/OperationsOnAutomata.jl
+++ src/OperationsOnAutomata.jl
@@ -1,4 +1,3 @@
-
 # completes the automaton, such that each state has an edge for each symbol in the alphabet.
 function complete!(A::automaton)
 
@@ -17,6 +16,37 @@ function complete!(A::automaton)
             if !haskey(s, c)
                 addEdge!(A, s, c, junkyard)
             end
+        end
+    end
+end
+
+# removes all unreachable states of an automaton
+function reduceNonAccessibleStates!(A::automaton)
+    reachableStates = Set{state}()
+    q = Queue{state}()
+
+    push!(reachableStates, A.initialState)
+    enqueue!(q, A.initialState)
+
+    # we use BFS on the graph of the automaton 
+    # and store all reached nodes
+    while !isempty(q)
+        s = dequeue!(q)
+        for c in A.alphabet
+            target = walkEdge(s, c)
+            if target ∉ reachableStates
+                push!(reachableStates, target)
+                enqueue!(q, target)
+            end
+        end
+    end
+
+    # finally, all unreached nodes are removed from the automaton.
+    # if they are referenced elsewhere, they survive.
+    # Otherwise the next Garbage Collection cycle catches them.
+    for s in A.states
+        if s ∉ reachableStates
+            removeState!(A, s)
         end
     end
 end
