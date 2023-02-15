82e9d4dc283a3aa8c9272650ea904758fa5aba0b
Jonas Seiler <Me@jonasseiler.de>
2023-02-15 10:08:52 +0100

    added Tests for building automata

--- test/Automaton.jl
+++ test/Automaton.jl
@@ -5,4 +5,29 @@
     @test a == b
 end
 
+@testset "building automata is correct" begin
+    a = Automata.automaton()
+    s = Automata.state("a")
+
+    Automata.addState!(a, s)
+
+    b = Automata.automaton(["epsilon", "a"], Vector{Char}(), "epsilon", Vector{String}(), Vector{Tuple{String,Char,String}}())
+
+    @test a == b
+
+    Automata.addTerminalState!(a, s)
+    b = Automata.automaton(["epsilon", "a"], Vector{Char}(), "epsilon", ["a"], Vector{Tuple{String,Char,String}}())
+
+    @test a == b
+
+    Automata.addSymbol!(a, 'a')
+    b = Automata.automaton(["epsilon", "a"], ['a'], "epsilon", ["a"], Vector{Tuple{String,Char,String}}())
+
+    @test a == b
+
+    Automata.addEdge!(a, "epsilon", 'a', "a")
+    b = Automata.automaton(["epsilon", "a"], ['a'], "epsilon", ["a"], [("epsilon", 'a', "a")])
+
+    @test a == b
+end
 
