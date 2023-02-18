2ffb0c92d594af022d1d163a3a626124bb7aa869
Jonas Seiler <Me@jonasseiler.de>
2023-02-18 17:08:49 +0100

    changed header to get linting for the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,9 @@
-#include("../src/Automata.jl")
+include("../src/Automata.jl")
 
-using Automata
+using .Automata
 using Test
 
 include("Automaton.jl")
 include("OperationsOnAutomata.jl")
-
-@testset "apfel" begin
-    a = automaton()
-
-    println(a)
-
-    @test a == a
-end
-
 
 # https://discourse.julialang.org/t/writing-tests-in-vs-code-workflow-autocomplete-and-tooltips/57488/8
