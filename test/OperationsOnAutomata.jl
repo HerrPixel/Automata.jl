432ec8c39b819b8ff823a4b0c3f3b088b0e0d834
Jonas Seiler <Me@jonasseiler.de>
2023-03-03 10:33:39 +0100

    added tests for intersection of automata

--- test/OperationsOnAutomata.jl
+++ test/OperationsOnAutomata.jl
@@ -1,215 +1,242 @@
 @testset "completing Automata" begin
-    a = automaton()
-    b = automaton()
+  a = automaton()
+  b = automaton()
 
-    addSymbol!(a, 'a')
-    addEdge!(a, "epsilon", 'a', "a")
+  addSymbol!(a, 'a')
+  addEdge!(a, "epsilon", 'a', "a")
 
-    addSymbol!(b, 'a')
-    addEdge!(b, "epsilon", 'a', "a")
+  addSymbol!(b, 'a')
+  addEdge!(b, "epsilon", 'a', "a")
 
-    #=
-        Both automata are:
-          ┌─┐       ┌─┐
-        ->│ε│ ─(a)─>│a│
-          └─┘       └─┘
-    =#
+  #=
+      Both automata are:
+        ┌─┐       ┌─┐
+      ->│ε│ ─(a)─>│a│
+        └─┘       └─┘
+  =#
 
-    @test a == b
-    @test isnothing(walkEdge(a.states["a"], 'a')) # edge does not exist
+  @test a == b
+  @test isnothing(walkEdge(a.states["a"], 'a')) # edge does not exist
 
-    complete!(a)
+  complete!(a)
 
-    #=
-        Now a is:
-          ┌─┐       ┌─┐       ┌────────┐─┐
-        ->│ε│ ─(a)─>│a│ ─(a)─>│junkyard│(a)
-          └─┘       └─┘       └────────┘<┘
-    =#
+  #=
+      Now a is:
+        ┌─┐       ┌─┐       ┌────────┐─┐
+      ->│ε│ ─(a)─>│a│ ─(a)─>│junkyard│(a)
+        └─┘       └─┘       └────────┘<┘
+  =#
 
-    @test a != b
-    @test !isnothing(walkEdge(a.states["a"], 'a')) # edge exists now
+  @test a != b
+  @test !isnothing(walkEdge(a.states["a"], 'a')) # edge exists now
 
-    complete!(b)
-    complete!(a)
+  complete!(b)
+  complete!(a)
 
-    #=
-        Both should still be:
-          ┌─┐       ┌─┐       ┌────────┐─┐
-        ->│ε│ ─(a)─>│a│ ─(a)─>│junkyard│(a)
-          └─┘       └─┘       └────────┘<┘
-    =#
+  #=
+      Both should still be:
+        ┌─┐       ┌─┐       ┌────────┐─┐
+      ->│ε│ ─(a)─>│a│ ─(a)─>│junkyard│(a)
+        └─┘       └─┘       └────────┘<┘
+  =#
 
-    @test a == b # completing an already completed automata changes nothing
+  @test a == b # completing an already completed automata changes nothing
 end
 
 @testset "reduce non accessible states" begin
-    a = automaton()
-    b = automaton()
+  a = automaton()
+  b = automaton()
 
-    addSymbol!(a, 'a')
-    addEdge!(a, "a", 'a', "epsilon")
+  addSymbol!(a, 'a')
+  addEdge!(a, "a", 'a', "epsilon")
 
-    addSymbol!(b, 'a')
-    addEdge!(b, "a", 'a', "epsilon")
+  addSymbol!(b, 'a')
+  addEdge!(b, "a", 'a', "epsilon")
 
-    #=
-        Both automata are:
-          ┌─┐       ┌─┐
-        ->│ε│<─(a)─ │a│
-          └─┘       └─┘
-    =#
+  #=
+      Both automata are:
+        ┌─┐       ┌─┐
+      ->│ε│<─(a)─ │a│
+        └─┘       └─┘
+  =#
 
-    @test a == b
-    @test semanticEquals(a, b)
+  @test a == b
+  @test semanticEquals(a, b)
 
-    reduceNonAccessibleStates!(a)
+  reduceNonAccessibleStates!(a)
 
-    #=
-        Now a is:
-          ┌─┐
-        ->│ε│
-          └─┘
-    =#
+  #=
+      Now a is:
+        ┌─┐
+      ->│ε│
+        └─┘
+  =#
 
-    @test a != b                # a has lost a state
-    @test semanticEquals(a, b)  # but semantically, a is still the same
+  @test a != b                # a has lost a state
+  @test semanticEquals(a, b)  # but semantically, a is still the same
 
-    reduceNonAccessibleStates!(b)
-    reduceNonAccessibleStates!(a)
+  reduceNonAccessibleStates!(b)
+  reduceNonAccessibleStates!(a)
 
-    @test a == b # reducing twice, changes nothing
+  @test a == b # reducing twice, changes nothing
 end
 
 @testset "accepting behaviour" begin
-    a = automaton()
+  a = automaton()
 
-    addSymbol!(a, 'a')
-    addEdge!(a, "epsilon", 'a', "a")
-    addTerminalState!(a, "a")
+  addSymbol!(a, 'a')
+  addEdge!(a, "epsilon", 'a', "a")
+  addTerminalState!(a, "a")
 
-    #=
-        a is:
-          ┌─┐      ╔═╗
-        ->│ε│─(a)─>║a║
-          └─┘      ╚═╝
-    =#
+  #=
+      a is:
+        ┌─┐      ╔═╗
+      ->│ε│─(a)─>║a║
+        └─┘      ╚═╝
+  =#
 
-    @test !isAccepted(a, "")
-    @test isAccepted(a, "a")
-    @test !isAccepted(a, "b") # words with symbols not in the alphabet
+  @test !isAccepted(a, "")
+  @test isAccepted(a, "a")
+  @test !isAccepted(a, "b") # words with symbols not in the alphabet
 
-    addTerminalState!(a, "epsilon")
+  addTerminalState!(a, "epsilon")
 
-    #=
-        a is:
-          ╔═╗      ╔═╗
-        ->║ε║─(a)─>║a║
-          ╚═╝      ╚═╝
-    =#
+  #=
+      a is:
+        ╔═╗      ╔═╗
+      ->║ε║─(a)─>║a║
+        ╚═╝      ╚═╝
+  =#
 
-    @test isAccepted(a, "") # correctly recognizes empty words
+  @test isAccepted(a, "") # correctly recognizes empty words
 end
 
 @testset "complement automata" begin
-    a = automaton()
+  a = automaton()
 
-    addSymbol!(a, 'a')
-    addEdge!(a, "epsilon", 'a', "a")
-    addTerminalState!(a, "a")
+  addSymbol!(a, 'a')
+  addEdge!(a, "epsilon", 'a', "a")
+  addTerminalState!(a, "a")
 
-    #=
-        a is:
-          ┌─┐      ╔═╗
-        ->│ε│─(a)─>║a║
-          └─┘      ╚═╝
-    =#
+  #=
+      a is:
+        ┌─┐      ╔═╗
+      ->│ε│─(a)─>║a║
+        └─┘      ╚═╝
+  =#
 
-    @test isTerminal(a, "a")
-    @test !isTerminal(a, "epsilon")
+  @test isTerminal(a, "a")
+  @test !isTerminal(a, "epsilon")
 
-    complement!(a)
+  complement!(a)
 
-    #=
-        a is:
-          ╔═╗      ┌─┐
-        ->║ε║─(a)─>│a│
-          ╚═╝      └─┘
-    =#
+  #=
+      a is:
+        ╔═╗      ┌─┐
+      ->║ε║─(a)─>│a│
+        ╚═╝      └─┘
+  =#
 
-    @test !isTerminal(a, "a")
-    @test isTerminal(a, "epsilon")
+  @test !isTerminal(a, "a")
+  @test isTerminal(a, "epsilon")
 end
 
 @testset "finding loops" begin
-    a = automaton()
+  a = automaton()
 
-    addSymbol!(a, 'a')
-    addEdge!(a, "epsilon", 'a', "a")
+  addSymbol!(a, 'a')
+  addEdge!(a, "epsilon", 'a', "a")
 
-    #=
-        a is:
-          ┌─┐       ┌─┐
-        ->│ε│ ─(a)─>│a│
-          └─┘       └─┘
-    =#
+  #=
+      a is:
+        ┌─┐       ┌─┐
+      ->│ε│ ─(a)─>│a│
+        └─┘       └─┘
+  =#
 
-    @test !hasLoop(a)
+  @test !hasLoop(a)
 
-    addEdge!(a, "a", 'a', "a")
+  addEdge!(a, "a", 'a', "a")
 
-    @test hasLoop(a)
+  @test hasLoop(a)
 
-    removeEdge!(a.states["a"], 'a')
-    addEdge!(a, "a", 'a', "aa")
-    addEdge!(a, "aa", 'a', "a")
+  removeEdge!(a.states["a"], 'a')
+  addEdge!(a, "a", 'a', "aa")
+  addEdge!(a, "aa", 'a', "a")
 
-    #=
-        Now a is:
-          ┌─┐       ┌─┐<─(a)─ ┌──┐
-        ->│ε│ ─(a)─>│a│       │aa│
-          └─┘       └─┘ ─(a)─>└──┘
-    =#
+  #=
+      Now a is:
+        ┌─┐       ┌─┐<─(a)─ ┌──┐
+      ->│ε│ ─(a)─>│a│       │aa│
+        └─┘       └─┘ ─(a)─>└──┘
+  =#
 
-    @test hasLoop(a)
+  @test hasLoop(a)
 end
 
 @testset "minmalizing automata" begin
-    a = automaton()
+  a = automaton()
 
-    addSymbol!(a, 'a')
-    addEdge!(a, "epsilon", 'a', "a")
-    addTerminalState!(a, "a")
-    addEdge!(a,"a",'a',"a")
+  addSymbol!(a, 'a')
+  addEdge!(a, "epsilon", 'a', "a")
+  addTerminalState!(a, "a")
+  addEdge!(a, "a", 'a', "a")
 
-    b = automaton()
+  b = automaton()
 
-    addSymbol!(b, 'a')
-    addEdge!(b, "epsilon", 'a', "a")
-    addTerminalState!(b, "a")
-    addEdge!(b,"a",'a',"a")
+  addSymbol!(b, 'a')
+  addEdge!(b, "epsilon", 'a', "a")
+  addTerminalState!(b, "a")
+  addEdge!(b, "a", 'a', "a")
 
-    #=
-        both automata are:
-          ┌─┐       ╔═╗─┐
-        ->│ε│ ─(a)─>║a║(a)
-          └─┘       ╚═╝<┘
-    =#
+  #=
+      both automata are:
+        ┌─┐       ╔═╗─┐
+      ->│ε│ ─(a)─>║a║(a)
+        └─┘       ╚═╝<┘
+  =#
 
-    a = minimalize(a)
+  a = minimalize(a)
 
-    @test semanticEquals(a,b) # minimalizing an already minimized automata changes nothing
+  @test semanticEquals(a, b) # minimalizing an already minimized automata changes nothing
 
-    addTerminalState!(a,a.initialState)
+  addTerminalState!(a, a.initialState)
 
-    #=
-        Now a is:
-          ╔═╗       ╔═╗─┐
-        ->║ε║ ─(a)─>║a║(a)
-          ╚═╝       ╚═╝<┘
-    =#
-    
-    a = minimalize(a)
+  #=
+      Now a is:
+        ╔═╗       ╔═╗─┐
+      ->║ε║ ─(a)─>║a║(a)
+        ╚═╝       ╚═╝<┘
+  =#
 
-    @test !semanticEquals(a,b) # a should be reduced to a single state now
+  a = minimalize(a)
+
+  @test !semanticEquals(a, b) # a should be reduced to a single state now
+end
+
+@testset "Intersection of automata" begin
+  a = automaton()
+  b = automaton()
+
+  addEdge!(a, "epsilon", 'a', "a")
+  addEdge!(a, "a", 'a', "aa")
+  addEdge!(a, "aa", 'a', "aa")
+  addTerminalState!(a, "aa")
+
+  addEdge!(b, "epsilon", 'a', "a")
+  addEdge!(b, "a", 'a', "epsilon")
+  addTerminalState!(b, "a")
+
+  c = Automata.Intersection(a, b)
+  minimalize(c)
+
+  d = automaton()
+
+  addEdge!(d, "epsilon", 'a', "a")
+  addEdge!(d, "a", 'a', "aa")
+  addEdge!(d, "aa", 'a', "aaa")
+  addEdge!(d, "aaa", 'a', "aa")
+  addTerminalState!(d, "aaa")
+
+  @test semanticEquals(c, d)
 end
