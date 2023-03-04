1d2fa00d95a662fc433dacb9e6db8d2156a7947e
Jonas Seiler <Me@jonasseiler.de>
2023-03-04 19:55:04 +0100

    exported new methods

--- src/Automata.jl
+++ src/Automata.jl
@@ -10,6 +10,6 @@ export addState!, addEdge!, addSymbol!, addTerminalState!
 export semanticEquals, walkEdge, isTerminal
 export removeState!, removeTerminalState!, removeEdge!
 
-export complete!, reduceNonAccessibleStates!, isAccepted, complement!, hasLoop, minimalize
+export complete!, reduceNonAccessibleStates!, isAccepted, complement!, hasLoop, minimalize, Intersection, Union, Concatenation
 
 end # module Automata
